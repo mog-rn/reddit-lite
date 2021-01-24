832c02a34dc94399054386b69022717bfef1b4ba
Amos Mogaka <mogaka.amo254@gmail.com>
2021-01-24 21:45:35 +0300

    commit

--- scripts/test.js
+++ scripts/test.js
@@ -16,7 +16,7 @@ process.on('unhandledRejection', err => {
 require('../config/env');
 
 
-
+const jest = require('jest');
 const execSync = require('child_process').execSync;
 let argv = process.argv.slice(2);
 
